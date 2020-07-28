2cdb26ffbded3a58b618feb7fa961a0cccf78efa
edsonmaia <edsonmaia2005@hotmail.com>
2020-07-28 09:08:32 -0300

    Atualização dia 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/home">
-                <img className="Logo" src={Logo} alt="Logo MaiaFlix"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
-                Novo vídeo
-            </Button>
-                
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
+
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
- 
+
 export default Menu;
