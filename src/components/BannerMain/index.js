8d2c87cfd21c7206aa65349a6bade39dc2032389
edsonmaia <edsonmaia2005@hotmail.com>
2020-08-04 22:09:17 -0300

    Update index.js

--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -35,7 +35,7 @@ export default function BannerMain({
           <VideoIframeResponsive
             youtubeID={youTubeID}
           />
-          <WatchButton>
+          <WatchButton as="a" href={url} target="_blank">
             Assistir
           </WatchButton>
         </ContentAreaContainer.Item>
