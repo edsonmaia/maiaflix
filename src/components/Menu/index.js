ac6d6c72b41e088878652b10e16b667fa1ef83a9
Edson Maia <edsonmaia2005@hotmail.com>
2020-10-22 10:46:53 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -10,12 +10,6 @@ function Menu() {
       <Link to="/">
         <img className="Logo" src={Logo} alt="MaiaFlix logo" />
       </Link>
-/*
-      <Button as={Link} className="ButtonLink" to="/cadastro/video">
-        Novo vídeo
-      </Button>
-    </nav>
-    */
   );
 }
 
